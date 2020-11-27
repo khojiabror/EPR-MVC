0ca8399aba9a08161194053d32a091f2815145c9
Khokhojiabrorjiabror Makhmu <abror@outlook.com>
2020-11-27 10:48:40 +0500

    [table-successfully-added]

---
+++ EPR-MVC/Controllers/PartsController.cs
@@ -0,0 +1,24 @@
+﻿using EPR_MVC.Models;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web.Mvc;
+
+namespace EPR_MVC.Controllers
+{
+    public class PartsController : Controller
+    {
+        // GET: Parts
+        public ActionResult Index()
+        {
+            using (DBEPREntities db = new DBEPREntities())
+            {
+                List<PART> partList = db.PARTS.ToList();
+
+                PartsViewModel pvm = new PartsViewModel();
+
+                List<PartsViewModel> partVMList = partList.Select(p => new PartsViewModel { Code = p.Code, Name = p.Name, Description = p.Description, PartNumber = p.PartNumber, NamePlate = p.NamePlate, MachineName = p.MACHINE.Name, SupplierName = p.SUPPLIER.Name, OEMName = p.OEM.Name, ManufacturerName = p.MANUFACTURER.Name, UzAutoSupplierName = p.UZAUTOSUPPLIER.Name }).ToList();
+                return View(partVMList);
+            }
+        }
+    }
+}
---
+++ EPR-MVC/Models/PartsViewModel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace EPR_MVC.Models
+{
+    public class PartsViewModel
+    {
+        public int ID { get; set; }
+        public string Code { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string PartNumber { get; set; }
+        public byte[] Photo { get; set; }
+        public string UzAutoSupplierName { get; set; }
+        public string NamePlate { get; set; }
+        public string ManufacturerName { get; set; }
+        public string SupplierName { get; set; }
+        public string OEMName { get; set; }
+        public string MachineName { get; set; }
+    }
+}
