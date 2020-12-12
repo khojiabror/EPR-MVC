e34c1398bb7164dc27ec9136a9c9a39e0fadfdb2
Khokhojiabrorjiabror Makhmu <abror@outlook.com>
2020-12-12 06:27:03 +0500

    12.12.2020 commit

--- EPR-MVC/Controllers/AdminController.cs
+++ EPR-MVC/Controllers/AdminController.cs
@@ -1,7 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using EPR_MVC.Models;
 using System.Linq;
-using System.Web;
 using System.Web.Mvc;
 
 namespace EPR_MVC.Controllers
@@ -10,6 +8,21 @@ namespace EPR_MVC.Controllers
     {
         // GET: Admin
         public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult Users()
+        {
+            using (DBEPREntities db = new DBEPREntities())
+            {
+
+                UserViewModel uvm = new UserViewModel();
+                uvm.userList = db.USERS.Include("UZAUTOSUPPLIER").Where(u => u.IsDeleted == false).ToList();
+
+                return View(uvm);
+            }
+        }
+        public ActionResult User()
         {
             return View();
         }
--- EPR-MVC/Models/UserViewModel.cs
+++ EPR-MVC/Models/UserViewModel.cs
@@ -1,23 +1,28 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
-using System.Linq;
-using System.Web;
 
 namespace EPR_MVC.Models
 {
     public class UserViewModel
     {
         public int ID { get; set; }
-        [Required(ErrorMessage ="Loginni kiriting")]
+        public int roleID { get; set; }
+        [Required(ErrorMessage = "Loginni kiriting")]
         public string Username { get; set; }
         public string Name { get; set; }
+        public string Email { get; set; }
         [Required(ErrorMessage = "Kalit so'zini kiriting")]
         public string Password { get; set; }
         public bool Status { get; set; }
+        public bool IsOnline { get; set; }
+        public string Telephone { get; set; }
+        public string Department { get; set; }
+        public string JobTitle { get; set; }
         //public string Question { get; set; }
         //public string Answer { get; set; }
         //public Nullable<bool> IsOnline { get; set; }
         public int UzautosupplierID { get; set; }
+        public List<USER> userList { get; set; }
+        public List<ROLE> roleList { get; set; }
     }
 }
